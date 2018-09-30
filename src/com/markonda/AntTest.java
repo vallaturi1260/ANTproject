cdebb3407a87e5a9bf34922017fba7fe8ca7e0aa
markonda reddy <bmarkondareddy@yahoo.com>
2018-09-30 17:26:58 +0530

    Add eclipse AntProject files

---
+++ src/com/markonda/AntTest.java
@@ -0,0 +1,26 @@
+package com.markonda;
+
+import java.util.HashMap;
+
+public class AntTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Ant program");
+
+		System.out.println("after svn configure in jenkins");
+
+		System.out.println("welcome to jenkins");
+		
+		System.out.println("added new things");
+		
+		System.out.println("locking");
+		
+		System.out.println("today I updated");
+		
+		HashMap<Integer, String> s=new HashMap<Integer,String>();
+		
+
+	}
+
+}
