aa94b3903049de75dd2b05693fe11eb90509fcef
vallaturi1260 <vallaturi1260@gmail.com>
2021-04-01 13:53:17 +0530

    deleted:    .settings/.jsdtscope 	deleted:    .settings/.svn/all-wcprops 	deleted:    .settings/.svn/entries 	deleted:    .settings/.svn/format 	deleted:    .settings/.svn/text-base/.jsdtscope.svn-base 	deleted:    .settings/.svn/text-base/org.eclipse.jdt.core.prefs.svn-base 	deleted:    .settings/.svn/text-base/org.eclipse.wst.common.component.svn-base 	deleted:    .settings/.svn/text-base/org.eclipse.wst.common.project.facet.core.xml.svn-base 	deleted:    .settings/.svn/text-base/org.eclipse.wst.jsdt.ui.superType.container.svn-base 	deleted:    .settings/.svn/text-base/org.eclipse.wst.jsdt.ui.superType.name.svn-base 	deleted:    .settings/org.eclipse.jdt.core.prefs 	deleted:    .settings/org.eclipse.wst.common.component 	deleted:    .settings/org.eclipse.wst.common.project.facet.core.xml 	deleted:    .settings/org.eclipse.wst.jsdt.ui.superType.container 	deleted:    .settings/org.eclipse.wst.jsdt.ui.superType.name 	modified:   build.xml 	modified:   build/classes/com/markonda/AntTest.class 	new file:   build/classes/com/markonda/Sample.class 	new file:   build/classes/com/markonda/Test1.class 	modified:   dest/AntTest.jar 	modified:   ears/AntBank.ear 	new file:   src/com/markonda/Sample.java 	new file:   src/com/markonda/Test1.java

---
+++ src/com/markonda/Sample.java
@@ -0,0 +1,13 @@
+package com.markonda;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Sample.java class");
+		
+
+	}
+
+}
---
+++ src/com/markonda/Test1.java
@@ -0,0 +1,12 @@
+package com.markonda;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Test class");
+
+	}
+
+}
